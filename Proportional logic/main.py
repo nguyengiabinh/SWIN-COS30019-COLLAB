45acab44a6e0c36d8731bcbea8e0891ea2b21e7c
nguyengiabinh <115540912+nguyengiabinh@users.noreply.github.com>
2024-04-03 01:31:51 +0700

    Revert "nevermind, all 3 test came back correct"
    This reverts commit 498b4f8d2a86dbd4b6792711d01a559bef7fd494.

--- Proportional logic/main.py
+++ Proportional logic/main.py
@@ -2,7 +2,7 @@ from File_Reader import *
 from knowledge_base import build_knowledge_base, print_knowledge_base
 
 # Read File
-filename = 'test3.txt' 
+filename = 'test.txt' 
 
 print(f'Debug filename: {filename}\n')
 
